1a8f1445df6ef6c7f7f9ed0f7852159e0b025a67
Nam Thanh Le <lethanhnam2882@gmail.com>
2018-11-15 00:53:57 +0700

    Add Dv, Qk models

---
+++ MaterialsManagement/Model/Dv.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialsManagement.Model
+{
+    public class Dv
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public Dv(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ MaterialsManagement/Model/Qk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialsManagement.Model
+{
+    public class Qk
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public Qk(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
