68ba4707005478c705585ae366af2e7f0ae808a7
Nam Thanh Le <lethanhnam2882@gmail.com>
2018-11-16 15:23:21 +0700

    Add Enums

---
+++ MaterialsManagement/Common/Enum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialsManagement.Common
+{
+    public enum MaterialEnum
+    {
+        [Display(Name = "Tàu, thuyền")]
+        Sail = 1,
+        [Display(Name = "Ô tô")]
+        Car = 2,
+        [Display(Name = "Vũ khí")]
+        Weapon = 3
+    }
+}
---
+++ MaterialsManagement/Common/EnumExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.ComponentModel.DataAnnotations;
+
+namespace MaterialsManagement.Common
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<DisplayAttribute>()
+                            .GetName();
+        }
+    }
+}
