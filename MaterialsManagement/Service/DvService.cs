df398f63c957fd4aa8af769c7cb75535b7df578c
Nam Thanh Le <lethanhnam2882@gmail.com>
2018-11-17 17:20:56 +0700

    Add Service classed for Qk and Dv

---
+++ MaterialsManagement/Service/DvService.cs
@@ -0,0 +1,23 @@
+﻿using MaterialsManagement.Model;
+using MaterialsManagement.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialsManagement.Service
+{
+    public class DvService
+    {
+        public List<Dv> GetAll()
+        {
+            return new DvRepository().GetAll();
+        }
+
+        public List<Dv> GetByQkId(string QkId)
+        {
+            return new DvRepository().GetByQkId(QkId);
+        }
+    }
+}
---
+++ MaterialsManagement/Service/QkService.cs
@@ -0,0 +1,22 @@
+﻿using MaterialsManagement.Model;
+using MaterialsManagement.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialsManagement.Service
+{
+    public class QkService
+    {
+        public List<Qk> GetAll()
+        {
+            return new QkRepository().GetAll();
+        }
+        public Qk Get(string Id)
+        {
+            return new QkRepository().Get(Id);
+        }
+    }
+}
